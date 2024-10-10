b00cab896378e0a83404c14257c773e8ee286c73
assoly-lab <81777632+assoly-lab@users.noreply.github.com>
2024-10-10 14:56:21 +0100

    added static to root urls.py

--- core/urls.py
+++ core/urls.py
@@ -23,7 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(endpoints)),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
